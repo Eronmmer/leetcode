b063294e8b883cfb5ffeb6b91296cfdccc2c5bd2
Erons <eronmmer@gmail.com>
2024-12-29 03:01:20 +0100

    add JS solution to leetcode #121

---
+++ sliding-window/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+// Leetcode #121
+
+var maxProfit = function (prices) {
+	let maxProfit = 0;
+	if (prices.length < 2) return 0;
+	let buyIdx = 0,
+		sellIdx = 1;
+	while (buyIdx < sellIdx && sellIdx < prices.length) {
+		const currProfit = prices[sellIdx] - prices[buyIdx];
+		maxProfit = Math.max(maxProfit, currProfit);
+		if (currProfit < 0) {
+			buyIdx = sellIdx;
+			sellIdx++;
+		} else {
+			sellIdx++;
+		}
+	}
+
+	return maxProfit;
+};
